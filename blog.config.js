a2fb3665d8ae1fca82d52ce9aca237a5d6421118
itxh <892457803@qq.com>
2024-12-27 20:32:35 +0800

    2024年12月27日:更新版本

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://wangdu.site',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion','文武科技社'],
+    keywords: ['Blog', 'Website', 'Notion', '文武科技社'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
